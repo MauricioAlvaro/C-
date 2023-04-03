54d4ce601eb1508fd51f9b8a8f5a156fad5115d1
MauricioAlvaro <maurialvarorc@gmail.com>
2023-04-03 12:27:06 -0400

    Add files via upload

---
+++ Hola Mundo/Hola Mundo/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Introduce tu nombre:");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola "+nombre);
+
+string hora = DateTime.Now.ToString("h:mm:ss tt");
+Console.WriteLine("La hora es : " + hora);
