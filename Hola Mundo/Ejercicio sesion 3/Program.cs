4124de16aa29e84b465fb27c675946e35f3ad934
MauricioAlvaro <maurialvarorc@gmail.com>
2023-04-03 18:49:21 -0400

    Add files via upload

---
+++ Hola Mundo/Ejercicio sesion 3/Program.cs
@@ -0,0 +1,20 @@
+﻿Cliente cliente1 = new Cliente("Mauricio Rodriguez", 123456, "Zona Alto san antonio", "asd@gmail.com", true);
+Console.WriteLine(cliente1);
+public struct Cliente
+{
+    public Cliente(string nombre, int numero, string direccion, string email, bool nuevoCliente)
+    {
+        Nombre = nombre;
+        Numero = numero;
+        Direccion = direccion;
+        Email = email;
+        NuevoCliente = nuevoCliente;
+    }
+    public string Nombre { get; }
+    public int Numero { get; }
+    public string Direccion { get; }
+    public string Email { get; }
+    public bool NuevoCliente { get; }
+
+    public override string ToString() =>$"{Nombre}, {Numero}, {Direccion}, {Email}, {NuevoCliente}";
+}
