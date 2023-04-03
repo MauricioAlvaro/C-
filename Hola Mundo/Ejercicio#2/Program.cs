7bdb251c4809d25d4c3e34a1d2375ab8b8300a41
MauricioAlvaro <maurialvarorc@gmail.com>
2023-04-03 17:11:37 -0400

    Add files via upload

---
+++ Hola Mundo/Ejercicio#2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese sus Datos personales");
+Console.Write("Ingrese su Nombre: ");
+string? nombre = Console.ReadLine();
+Console.Write("Ingrese su Apellido: ");
+string? apellido = Console.ReadLine();
+Console.Write("Ingrese su Edad: ");
+int? edad = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("¿Sabe Programar? (True or False)");
+bool? programar = Convert.ToBoolean(Console.ReadLine()); 
+
+Console.WriteLine("Bienvenido " + nombre + " " + apellido);
+Console.WriteLine("Tienes " + edad + "años") ;
+Console.WriteLine("Sabe programar :" + programar);
