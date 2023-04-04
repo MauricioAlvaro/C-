3e9a038733ce1ea6b8c0b2d7b8d9cb483c5cde82
MauricioAlvaro <maurialvarorc@gmail.com>
2023-04-04 16:00:04 -0400

    Add files via upload

--- Hola Mundo/Ejercicio sesion 3/Program.cs
+++ Hola Mundo/Ejercicio sesion 3/Program.cs
@@ -1,20 +1,73 @@
-﻿Cliente cliente1 = new Cliente("Mauricio Rodriguez", 123456, "Zona Alto san antonio", "asd@gmail.com", true);
-Console.WriteLine(cliente1);
-public struct Cliente
+﻿//While
+/*Console.WriteLine("Ingrese un numero:");
+int numero = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+while (a <= 10)
 {
-    public Cliente(string nombre, int numero, string direccion, string email, bool nuevoCliente)
+    Console.WriteLine(numero+" x "+a+" = "+(a*numero));
+    a++;
+};
+//Do while
+int num = 0;
+int pos = 0;
+int neg = 0;
+do
+{
+    Console.WriteLine("Ingrese un numero:");
+    num = Convert.ToInt32(Console.ReadLine());
+    if (num > 0)
     {
-        Nombre = nombre;
-        Numero = numero;
-        Direccion = direccion;
-        Email = email;
-        NuevoCliente = nuevoCliente;
+        pos += 1;
     }
-    public string Nombre { get; }
-    public int Numero { get; }
-    public string Direccion { get; }
-    public string Email { get; }
-    public bool NuevoCliente { get; }
+    else if (num != 0)
+    {
+        neg += 1;
+    }
+} while (num != 0);
+Console.WriteLine("Números Positivos :" + pos);
+Console.WriteLine("Números Negativos :" + neg);*/
+//For
+Console.WriteLine("Ingresa 4 datos");
+Console.Write("Ingresa la cantidad :");
+int cantidad = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ingresa el alto :");
+int alto = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Ingresa el ancho :");
+int ancho= Convert.ToInt32(Console.ReadLine());
+Console.Write("Ingresa el relleno (true o false) :");
+bool relleno = Convert.ToBoolean(Console.ReadLine());
+string[,] matriz = new string[alto,ancho];
 
-    public override string ToString() =>$"{Nombre}, {Numero}, {Direccion}, {Email}, {NuevoCliente}";
+for(int k = 0; k < cantidad; k++)
+{
+    for (int i = 0; i < alto; i++)
+    {
+        for (int j = 0; j < ancho; j++)
+        {
+            if (relleno == true)
+            {
+                matriz[i, j] = "*";
+            }
+            else if ((i == 0 || j == 0) || (i == alto - 1 || j == ancho - 1))
+            {
+                matriz[i, j] = "*";
+            }
+            else
+            {
+                matriz[i, j] = " ";
+            }
+        }
+    }
+}
+for(int k = 0; k < cantidad; k++)
+{
+    for (int i = 0; i < alto; i++)
+    {
+        for (int j = 0; j < ancho; j++)
+        {
+            Console.Write(matriz[i, j]);
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
 }
